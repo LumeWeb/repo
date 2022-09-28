21b118b39494e0a1887f7a6cecac32d0f4c204cf
Derrick Hammer <derrick@derrickhammer.com>
2022-09-28 18:09:38 -0400

    *Initial version

---
+++ scripts/postinstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+DISTRO=$(lsb_release -cs)
+echo "deb [signed-by=/usr/share/keyrings/lumeweb.gpg] https://apt.web3relay.io/ubuntu ${DISTRO} main" | tee /etc/apt/sources.list.d/lumeweb.list
---
+++ scripts/postremove.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+rm -f /etc/apt/sources.list.d/lumeweb.list
