b2de28e4dbe0829b1ef7f618f819eb7ea5636d18
Derrick Hammer <derrick@derrickhammer.com>
2022-09-28 18:26:10 -0400

    *Don't use tee

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 DISTRO=$(lsb_release -cs)
-echo "deb [signed-by=/usr/share/keyrings/lumeweb.gpg] https://apt.web3relay.io/ubuntu ${DISTRO} main" | tee /etc/apt/sources.list.d/lumeweb.list
+echo "deb [signed-by=/usr/share/keyrings/lumeweb.gpg] https://apt.web3relay.io/ubuntu ${DISTRO} main" > /etc/apt/sources.list.d/lumeweb.list
